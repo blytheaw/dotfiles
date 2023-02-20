8e2d4208b557419130849bb3b9a7e7caa23de260
blytheaw <blythe.austinw@gmail.com>
2023-02-20 12:26:42 -0600

    neotree follow file

--- .config/nvim/after/plugin/neotree.lua
+++ .config/nvim/after/plugin/neotree.lua
@@ -1,5 +1,8 @@
 require('neo-tree').setup({
-    close_if_last_window = true
+    close_if_last_window = true,
+    filesystem = {
+        follow_current_file = true
+    }
 })
 
 vim.keymap.set("n", "<leader>b", '<cmd>Neotree<cr>')
